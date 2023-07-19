f543f842a07dac01502573dfa248ea67d2ae0e38
gdejong <gijs_d.j@hotmail.com>
2023-07-19 22:07:16 +0200

    Implement delete

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ A small Go project to get to know the [GORM](https://gorm.io/) library.
 
 ## How to run
 
+Create new users:
+
 ```shell
 go run . create
+```
+
+Delete users:
+
+```shell
+go run . delete <id>
 ```
--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"os"
+	"strconv"
 )
 
-const dsn = "test.db"
-const commandCreate = "create"
+const (
+	dsn = "test.db"
+
+	commandCreate = "create"
+	commandDelete = "delete"
+)
 
 func main() {
 	// Create a logger.
@@ -17,7 +22,7 @@ func main() {
 	logrus.SetOutput(os.Stdout)
 
 	// Make sure we have a command line argument.
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		logger.Info("Usage: go run . <command>")
 		os.Exit(1)
 	}
@@ -37,10 +42,33 @@ func main() {
 		panic("failed to auto migrate: " + err.Error())
 	}
 
-	if command == commandCreate {
+	switch command {
+	case commandCreate:
 		handleCreate(db, logger)
+	case commandDelete:
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			logger.WithError(err).Fatal("Failed to parse id")
+		}
+		handleDelete(db, logger, id)
 	}
 
+}
+
+func handleDelete(db *gorm.DB, logger *logrus.Logger, id int) {
+	res := db.Delete(&User{}, id)
+
+	if res.Error != nil {
+		logger.WithError(res.Error).Fatal("Failed to delete user")
+	}
+
+	if res.RowsAffected == 0 {
+		logger.WithField("id", id).Warn("User not found")
+
+		return
+	}
+
+	logger.WithField("id", id).Info("Deleted user")
 }
 
 func handleCreate(db *gorm.DB, logger *logrus.Logger) {
