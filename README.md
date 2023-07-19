cbd1d236c9e3ca7f931128b1a169e8854d814a79
gdejong <gijs_d.j@hotmail.com>
2023-07-19 21:28:40 +0200

    Create users

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A small Go project to get to know the [GORM](https://gorm.io/) library.
 ## How to run
 
 ```shell
-go run .
+go run . create
 ```
--- main.go
+++ main.go
@@ -8,12 +8,22 @@ import (
 )
 
 const dsn = "test.db"
+const commandCreate = "create"
 
 func main() {
+	// Create a logger.
 	logger := logrus.New()
 	logrus.SetOutput(os.Stdout)
 
-	logger.Info("Opening database")
+	// Make sure we have a command line argument.
+	if len(os.Args) != 2 {
+		logger.Info("Usage: go run . <command>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+
+	logger.WithField("dsn", dsn).Info("Opening database")
 	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database: " + err.Error())
@@ -26,4 +36,21 @@ func main() {
 		panic("failed to auto migrate: " + err.Error())
 	}
 
+	if command == commandCreate {
+		handleCreate(db, logger)
+	}
+
+}
+
+func handleCreate(db *gorm.DB, logger *logrus.Logger) {
+	user := &User{
+		Name: "Klaas",
+	}
+	res := db.Create(user)
+
+	if res.Error != nil {
+		logger.WithError(res.Error).Fatal("Failed to create user")
+	}
+
+	logger.WithField("ID", user.ID).Info("Created user")
 }
